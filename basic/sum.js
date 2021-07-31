99036c446c95ad0f8b4cc1bce4ba2c8cfdfcf561
abbychen0427 <abbychen0427@gmail.com>
2021-07-31 14:21:01 +0800

    sum.js better version

--- basic/sum.js
+++ basic/sum.js
@@ -1,12 +1,16 @@
 console.log("Hello World")
 
+//O(1) better version
 function sum(n){
-    var total= 0;
-    for(let i=0; i<=n; i++){
-        total+= i;
-    }
-    return total;
+    return ((n+1)*n)/2;
 }
+// function sum(n){
+//     var total= 0;
+//     for(let i=0; i<=n; i++){
+//         total+= i;
+//     }
+//     return total;
+// }
 
 console.log(sum(3));
 console.log(sum(10));
