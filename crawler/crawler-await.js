53cea11cea0b19d0db1c359c9e64cbf8ef71a167
abbychen0427 <abbychen0427@gmail.com>
2021-08-07 20:57:26 +0800

    crawler-await.js prettier version

--- crawler/crawler-await.js
+++ crawler/crawler-await.js
@@ -14,19 +14,22 @@ async function getStockData() {
       });
     });
     // console.log(stockCode);
-    let response = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-      params: {
-        response: JSON,
-        date: moment().format("YYYYMMDD"),
-        stockNo: stockCode,
-      },
-    });
+    let response = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        params: {
+          response: JSON,
+          date: moment().format("YYYYMMDD"),
+          stockNo: stockCode,
+        },
+      }
+    );
     console.log(response.data);
-    }catch(err){
-        console.log(err)
-    }finally{
-        console.log("the end")
-    }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("the end");
+  }
 }
 
 getStockData();
