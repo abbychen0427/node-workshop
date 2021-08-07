46effcedc20d1c06eab585d9c79f56958486a10b
abbychen0427 <abbychen0427@gmail.com>
2021-08-07 16:07:39 +0800

    install moment and add crawler1.js

---
+++ crawler/crawler1.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const moment = require("moment");
+
+//api網址
+//https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210807&stockNo=2330
+
+//比較醜的寫法
+// axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210807&stockNo=2330")
+// .then((response) => {
+// console.log(response.data);
+// })
+//prettier
+axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+    params: {
+        response: JSON,
+        date: moment().format("YYYYMMDD"),
+        stockNo: 2330
+    }
+})
+.then((response) => {
+console.log(response.data);
+})
