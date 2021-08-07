136df35afb07b1addf2af1a2c4268684351dfab0
abbychen0427 <abbychen0427@gmail.com>
2021-08-07 20:47:36 +0800

    add crawler-await.js

---
+++ crawler/crawler-await.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
+
+async function getStockData() {
+  try {
+    let stockCode = await new Promise((resolve, reject) => {
+      fs.readFile("stock.txt", "utf-8", (err, stockCode) => {
+        if (err) {
+          reject("有錯誤", err);
+        } else {
+          resolve(stockCode);
+        }
+      });
+    });
+    // console.log(stockCode);
+    let response = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: JSON,
+        date: moment().format("YYYYMMDD"),
+        stockNo: stockCode,
+      },
+    });
+    console.log(response.data);
+    }catch(err){
+        console.log(err)
+    }finally{
+        console.log("the end")
+    }
+}
+
+getStockData();
