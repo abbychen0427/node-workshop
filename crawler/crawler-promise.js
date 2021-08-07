8d77df54f66ac691bd376b8f4f0f75d0744d26c9
abbychen0427 <abbychen0427@gmail.com>
2021-08-07 20:02:27 +0800

    add crawler-promise.js

---
+++ crawler/crawler-promise.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
+
+let stockCode = "";
+new Promise((resolve, reject) => {
+  fs.readFile("stock.txt", "utf-8", (err, stockCode) => {
+    if (err) {
+      reject("有錯誤", err);
+    } else {
+      resolve(stockCode);
+    }
+  });
+})
+  .then((stockCode) => {
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: JSON,
+        date: moment().format("YYYYMMDD"),
+        stockNo: stockCode,
+      },
+    });
+  })
+  .then((response) => {
+    console.log(response.data.title);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
