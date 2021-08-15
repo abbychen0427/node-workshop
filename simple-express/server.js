5265b066a0f1dbf8afcfc60ab0af1686d497f84b
abbychen0427 <abbychen0427@gmail.com>
2021-08-15 11:36:59 +0800

    add 2 middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,13 +3,31 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
+//使用中間件
+app.use((req, res, next) => {
+  let current = new Date();
+  console.log(`拜訪網站時間: ${current.toISOString()}`);
+  //不加next就會一直pending卡住
+  next();
+})
+
+app.use((req, res, next) => {
+  let current = new Date();
+  console.log("第二個中間件");
+  next();
+})
+
 // HTTP Method: get, post, put, patch, delete
 app.get("/", function (request, response, next) {
   response.send("Hello");
 });
 
 app.get("/about", function (request, response, next) {
-  response.send("about me");
+  response.send("about me1");
+});
+//在express上下關係明確 因此router重複的話，只會跑上面那一個
+app.get("/about", function (request, response, next) {
+  response.send("about me2");
 });
 
 app.listen(3000, function () {
