a738c67eaf60333d5899a772ad03ee69897e01ed
abbychen0427 <abbychen0427@gmail.com>
2021-08-23 16:36:28 +0800

    改用pool連線

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connection = require("./utils/db");
 
 // 利用 express 建立了一個 express application
 let app = express();
@@ -30,6 +31,20 @@ app.get("/about", function (request, response, next) {
   response.send("about me2");
 });
 
-app.listen(3000, function () {
+//stock get api
+app.get("/stock", async function (request, response, next) {
+  let result = await connection.queryAsync("SELECT * FROM stock");
+  response.json(result);
+});
+
+app.get("/stock/:stockCode", async (req, res, next) => {
+  let result = await connection.queryAsync("SELECT * FROM stock_price WHERE stock_id = ?", [req.params.stockCode]);
+  res.json(result);
+})
+
+app.listen(3000, async function () {
+  //啟動時建立好資料庫連結
+  // await connection.connectAsync();
+  //用pool會自動建立連線 不需要再額外打上面這行
   console.log("我們的 web server 啟動了～");
 });
--- simple-express/utils/db.js
+++ simple-express/utils/db.js
@@ -2,12 +2,14 @@ const mysql = require('mysql');
 require("dotenv").config();
 const Promise = require("bluebird");
 
-let connection = mysql.createConnection({
+let connection = mysql.createPool({
   host: process.env.DB_HOST, 
   port: process.env.DB_PORT,
   user: process.env.DB_USER, 
   password: process.env.DB_PASS, 
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  //設定連線上限預設值 用或(||)的方式
+  connectionLimit: process.env.CONNECTION_LIMIT || 10,
 });
 
 connection = Promise.promisifyAll(connection);
